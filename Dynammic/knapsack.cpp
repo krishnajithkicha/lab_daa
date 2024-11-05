19396cc340a8e19591a1968b8fb2abd9f32e3a39
Krishna Ajith <142655890+krishnajithkicha@users.noreply.github.com>
2024-11-05 17:50:53 +0530

    Update knapsack.cpp

--- Dynammic/knapsack.cpp
+++ Dynammic/knapsack.cpp
@@ -8,34 +8,63 @@ int max(int a,int b){
 		return b;
 	}
 }
-int knapsack(int W,int weight[],int val[],int n){
-	int F[n+1][W+1];
-	for(int i=0;i<=n+1;i++){
-		for(int j=0;j<=W+1;j++){
-			if(i==0 || j==0){
-				F[i][j]=0;
-			}
-			else if(weight[i-1]<=j){
-				F[i][j]=max(F[i-1][j],val[i-1]+F[i-1][j-weight[i-1]]);
+int knapsack(int c,int n,int w[],int v[]){
+	int F[n+1][c+1];
+	for(int i=0;i<n+1;i++){
+		F[i][0]=0;
+	}
+	for(int j=0;j<c+1;j++){
+		F[0][j]=0;
+	}
+	for(int i=1;i<n+1;i++){
+		for(int j=1;j<c+1;j++){
+			if(j-w[i-1]>=0){
+				F[i][j]=max(F[i-1][j],v[i-1]+F[i-1][j-w[i-1]]);
 			}
 			else{
 				F[i][j]=F[i-1][j];
 			}
 		}
 	}
-	for(int k=0;k<n+1;k++){
-		for(int l=0;l<W+1;l++){
-			cout<<F[k][l]<<" ";
+	for(int i=0;i<n+1;i++){
+		for(int j=0;j<c+1;j++){
+			cout<<F[i][j]<<" ";
 		}
 		cout<<endl;
 	}
-	return F[n][W];
+	int B[10];
+	int itc=0;
+	int cp=c;
+	for(int i=n;i>0;i--){
+		if(F[i][cp]!=F[i-1][cp]){
+			B[itc++]=i-1;
+			cp-=w[i-1];
+		}
+	}
+	cout<<endl;
+	cout<<"Backtrack array";
+	for(int i=itc-1;i>=0;i--){
+		cout<<B[i]<<" ";
+	}
+	cout<<endl;
+	return F[n][c];
 }
 int main(){
-	int W=5;
-	int val[]={12,10,20,15};
-	int weight[]={2,1,3,2};
-	int n=4;
-	int max=knapsack(W,weight,val,n);
-	cout<<max<<endl;
+	int n,c;
+	cout<<"Enter the number of items:";
+	cin>>n;
+	int v[n],w[n];
+	cout<<"Enter the values:";
+	for(int i=0;i<n;i++){
+		cin>>v[i];
+	}
+	cout<<"Enter the weights:";
+	for(int i=0;i<n;i++){
+		cin>>w[i];
+	}
+	cout<<"Enter the capacity:";
+	cin>>c;
+	int k=knapsack(c,n,w,v);
+	cout<<"max capacity:"<<k<<endl;
+	return 0;
 }
